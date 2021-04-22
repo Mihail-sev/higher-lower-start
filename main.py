475eb624ed4b28f83237879ebe483d14d325614a
Mihail Shmatok <>
2021-04-22 08:40:08 +0000

    1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ first = data[randint(0, len(data)-1)]
 second = data[randint(0, len(data)-1)]
 while second == first:
   second = data[randint(0, len(data)-1)]
+anti_global = [first, second, score]
 
 def print_all():
   print (f"\nCompare A: {first['name']}, a {first['description']}, from {first['country']}.")
@@ -17,13 +18,13 @@ def print_all():
   print (f"\nCompare B: {second['name']}, a {second['description']}, from {second['country']}.")
   answer = input("Who has more followers? Type 'A' or 'B':").upper()
   return answer
-def next_step():
-  global first, second, score
+def next_step(first, second, score):
   first = second
   second = data[randint(0, len(data)-1)]
   while second == first:
     second = data[randint(0, len(data)-1)]
   score += 1
+  return [first, second, score]
 def compare_followers():
   if  first['follower_count'] > second['follower_count']:
     return "A" 
@@ -39,7 +40,10 @@ while game_continue:
     print (logo, "\n"f"Sorry, that's wrong. Final score: {score}")
     print (answer)
   elif answer == compare_followers():
-    next_step()
+    anti_global = next_step(first, second, score)
+    first = anti_global[0]
+    second = anti_global[1]
+    score = anti_global[2]
     clear()
     print (logo, "\n" f"You are right. Current score: {score}")
     answer = print_all()
